e30bcaea653bc14d7eb601464c2496b67c75efce
Irwin Herridge <irwinherridge3@gmail.com>
2021-01-14 11:04:45 +0200

    Added maven dependencies for web app and added exception handling on the conversion service

---
+++ service-conversions/src/main/java/za/co/iherridge0/rest/webservices/conversionsrest/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,26 @@
+package za.co.iherridge0.rest.webservices.conversionsrest.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import za.co.iherridge0.rest.webservices.conversionsrest.entity.ExceptionResponse;
+
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
--- web-app/src/main/resources/static/src/routes.js
+++ web-app/src/main/resources/static/src/routes.js
@@ -17,13 +17,13 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
 
     .state('login', {
       url: '/login',
-      templateUrl: 'js/login/login.template.html',
+      templateUrl: 'src/login/login.template.html',
       controller: 'LoginController as $ctrl'
     })
 
     .state('conversions', {
       url: '/conversions',
-      templateUrl: 'js/conversions/conversions.template.html',
+      templateUrl: 'src/conversions/conversions.template.html',
       controller: 'ConversionsController as $ctrl'
     });
 }
